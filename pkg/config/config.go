3f1d7a802c4a21899a7359ecb1cdbe7db7950c75
Frank Machado Graziadio <38917076+infinityxgame@users.noreply.github.com>
2026-09-17 23:10:49 -0400

    v2.4.9

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	AppVersion = "2.4.8"
+	AppVersion = "2.4.9"
 	GithubRepo = "infinityxgame/tgdown"
 
 	// DefaultBindHost es la dirección en la que escucha el panel cuando el
